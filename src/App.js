b13050c0e64e63b571b323343d55f10771a81ccb
Muna <mmhussein125@gmail.com>
2018-12-29 17:58:38 +0000

    Listens for user input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar';
 
 class App extends Component {
+  handleQueryChange(query) {
+    console.log(query);
+  }
+
   render() {
     return (
-      <div>
-        <SearchBar/>
+      <div className="App">
+        <SearchBar onQueryChange={this.handleQueryChange} />
       </div>
     );
   }
