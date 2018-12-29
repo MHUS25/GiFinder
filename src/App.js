c8621b8b2ac6bb31d330b88668f6a51b9979849f
Muna <mmhussein125@gmail.com>
2018-12-29 19:12:47 +0000

    App renders GifList with some hardcoded gifs for now

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import GifList from './components/GifList';
+
 
 class App extends Component {
+  constructor() {
+        super();
+
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'https://media.giphy.com/media/59R6pXgFbvdny/giphy.gif'
+                },
+                {
+                    id: 2,
+                    url: 'https://media.giphy.com/media/xUA7beaG2jNv74K9ry/giphy.gif'
+                },
+                {
+                    id: 3,
+                    url: 'https://media.giphy.com/media/3o7qDFZCe1ZUfHRLvq/giphy.gif'
+                }
+            ]
+        }
+    }
+
   handleQueryChange(query) {
     console.log(query);
   }
@@ -13,6 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBar onQueryChange={this.handleQueryChange} />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
