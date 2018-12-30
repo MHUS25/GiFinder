dffa9349b8a6535fc695bd398fc6dc46ed595535
Muna <mmhussein125@gmail.com>
2018-12-30 00:44:06 +0000

    Fetch requesr to get gif from giphy api

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
-
 
 class App extends Component {
   constructor() {
         super();
 
         this.state = {
-            gifs: [
-                {
-                    id: 1,
-                    url: 'https://media.giphy.com/media/59R6pXgFbvdny/giphy.gif'
-                },
-                {
-                    id: 2,
-                    url: 'https://media.giphy.com/media/xUA7beaG2jNv74K9ry/giphy.gif'
-                },
-                {
-                    id: 3,
-                    url: 'https://media.giphy.com/media/3o7qDFZCe1ZUfHRLvq/giphy.gif'
-                }
-            ]
+            gifs: []
         }
     }
 
   handleQueryChange(query) {
-    console.log(query);
+    const url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=txUDYZkPAy3Xcj0ocBlnlqjT0RX8nwu3`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res.data[0]);
+          })
   }
+
+
 
   render() {
     return (
