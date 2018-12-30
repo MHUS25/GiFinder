c8db7cd7e2ea5ef1078f6a896ff0b1c14a735e85
Muna <mmhussein125@gmail.com>
2018-12-30 05:29:01 +0000

    Adds alt property to images

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GifItem = (image) => {
   return (
     <ul>
-      <img src={image.gif.images.downsized.url} />
+      <img src={image.gif.images.downsized.url} alt="" />
     </ul>
   )
 };
