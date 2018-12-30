9f5473e53fcc3198140b38701095e1d7beeb62be
Muna <mmhussein125@gmail.com>
2018-12-30 14:36:53 +0000

    Basic styling

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GifItem = (image) => {
   return (
-    <ul>
+    <ul className="gif-item">
       <img src={image.gif.images.downsized.url} alt="" />
     </ul>
   )
