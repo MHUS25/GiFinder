10a06a7ac2f3af06903e90bc84a265b3b7c010e7
Muna <mmhussein125@gmail.com>
2018-12-31 21:45:57 +0000

    Redux set up with some hardcoded GIFs

---
+++ src/reducers/gifs.js
@@ -0,0 +1,16 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      url: 'https://media.giphy.com/media/3o751Yxe9UjX26BZbG/giphy.gif'
+    },
+    {
+      id: 2,
+      url: 'https://media.giphy.com/media/12sHg8v0G84V20/giphy.gif'
+    },
+    {
+      id: 3,
+      url: 'https://media.giphy.com/media/xT0xeojepOgOi0PPzi/giphy.gif'
+    }
+  ];
+}
