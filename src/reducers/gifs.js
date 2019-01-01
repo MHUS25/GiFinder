c225bdd65c4590796d33d2c378e8316b783b066f
Muna <mmhussein125@gmail.com>
2019-01-01 19:29:38 +0000

    Rewrites reducer to link to action

--- src/reducers/gifs.js
+++ src/reducers/gifs.js
@@ -1,16 +1,16 @@
-export default function() {
-  return [
-    {
-      id: 1,
-      url: 'https://media.giphy.com/media/3o751Yxe9UjX26BZbG/giphy.gif'
-    },
-    {
-      id: 2,
-      url: 'https://media.giphy.com/media/12sHg8v0G84V20/giphy.gif'
-    },
-    {
-      id: 3,
-      url: 'https://media.giphy.com/media/xT0xeojepOgOi0PPzi/giphy.gif'
-    }
-  ];
+import { REQUEST_GIFS } from '../actions';
+
+const initialState =  {
+  data: []
+};
+
+export default function gifs(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_GIFS:
+      return {
+        ...state, data: action.payload.body.data
+      };
+    default:
+      return state;
+  }
 }
