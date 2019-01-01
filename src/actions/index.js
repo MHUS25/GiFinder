9c286d6bc32991ad4ce849a1f9cac5e8b5087c98
Muna <mmhussein125@gmail.com>
2019-01-01 19:10:43 +0000

    Returnin array of GIFs using superagent AJAX library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
+import request from 'superagent';
+
 export const REQUEST_GIFS = 'REQUEST_GIFS';
 
+const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const API_KEY = '&api_key=txUDYZkPAy3Xcj0ocBlnlqjT0RX8nwu3';
+
 export function requestGifs(query = null) {
-  console.log(query);
+  const data = request.get(`${API_URL}${query.replace(/\s/g, '+')}${API_KEY}`);
   return {
     type: REQUEST_GIFS,
-    query
+    payload: data
   }
 }
